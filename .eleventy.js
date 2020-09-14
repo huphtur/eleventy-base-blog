8008323ebcb29fdaf91e1db40cdcf3a6ba11b4d9
M. Appelman <huphtur@icloud.com>
2020-09-14 09:22:45 -0700

    remove markdownit dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,6 @@ const fs = require("fs");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
 const pluginEmbedTweet = require("eleventy-plugin-embed-tweet");
 
 module.exports = function(eleventyConfig) {
@@ -71,18 +69,6 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
-
-  /* Markdown Overrides */
-  let markdownLibrary = markdownIt({
-    html: true,
-    breaks: true,
-    linkify: false
-  }).use(markdownItAnchor, {
-    permalink: true,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "#"
-  });
-  eleventyConfig.setLibrary("md", markdownLibrary);
 
   // Browsersync Overrides
   eleventyConfig.setBrowserSyncConfig({
